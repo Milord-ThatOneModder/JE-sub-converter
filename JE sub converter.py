24390c4a81cf5674ef7da6982a0b58da82a23ec6
jakubluc <m1l0rd30467@gmail.com>
2022-11-08 08:53:35 +0100

    fixed things, code beatification

--- JE sub converter.py
+++ JE sub converter.py
@@ -7,6 +7,7 @@ import os
 
 def add_by_id(job, waypoints, displacement):
     # job = 'commanding_officer'
+    # TODO check if waypoint wint end up in a fucking wall or worse, outside. For now its user problem :barodev:
     x = str(int(waypoints['x']) + displacement)
     y = waypoints['y']
     idcards = waypoints['idcardtags']
@@ -19,6 +20,7 @@ def add_by_id(job, waypoints, displacement):
 # things to import:
 # Submarine name
 
+# jobs to replace,  assistant split to other classes and awaiting rework
 newJobs = [
     'commanding_officer', 'executive_officer', 'navigator', 'chief',
     'engineering', 'mechanical', 'quartermaster', 'head_of_security',
@@ -42,33 +44,33 @@ def main():
     filename  = "Azimuth [JE].xml"
     changename = False
     if(len(sys.argv) > 1):
-        if(str(sys.argv[1][-4]) + str(sys.argv[1][-3]) + str(sys.argv[1][-2]) + str(sys.argv[1][-1]) == '.sub' and os.path.exists(sys.argv[1])):
-            filename = sys.argv[1]
-            with gzip.open(filename, 'rb') as f:
-                file_content = f.read()
+        for i in range(1,len(sys.argv)):
+            # IndexError: string index out of range for some thus this:
+            if(len(sys.argv[i]) > 4):
+                if(str(sys.argv[i][-4]) + str(sys.argv[i][-3]) + str(sys.argv[i][-2]) + str(sys.argv[i][-1]) == '.sub' and os.path.exists(sys.argv[i])):
+                    filename = sys.argv[i]
+                    with gzip.open(filename, 'rb') as f:
+                        file_content = f.read()
+                # TODO support for more than one 'filename' or/and detect if user screwed up and typed sub twice. Also less supid check
 
-                filename = filename[:-4] + ".xml"
-                with open(filename + ".sub", 'wb') as f:
-                        f.write(file_content)
+                        filename = filename[:-4] + ".xml"
+                        with open(filename, 'wb') as fx:
+                                fx.write(file_content)
 
-    if len(sys.argv) >= 3 :
-        if sys.argv == '--changename' or sys.argv == '-c':
-            changename = True
+            if len(sys.argv) >= 3 :
+                if sys.argv[2] == '--changename' or sys.argv[2] == '-c':
+                    changename = True
+    # else:
+    # TODO a propt for user imput if no arguents are given
 
-    # mydoc = minidom.parse(filename)
+    # parse an xml file by name
     with minidom.parse(filename) as mydoc:
-
-        # parse an xml file by name
-        
-
         items = mydoc.getElementsByTagName('Submarine')[0]
-
-        #cant aqqure this
-        #previewimage = items.getAttribute('previewimage')
 
         requiredcontentpackages = items.getAttribute('requiredcontentpackages')
         name = items.getAttribute('name')
         description = items.getAttribute('description')
+        #cant print this as variable so whatever
         previewimage = items.getAttribute('previewimage')
 
         # one specific item attribute
@@ -77,34 +79,30 @@ def main():
         print('Item requiredcontentpackages attribute: ' + requiredcontentpackages)
 
         previewimage = base64.b64decode(previewimage)
-
         name_ofpic = name + ".png"
-
         image_result = open(name_ofpic, 'wb')
         print("Item previewimage attribute: " + "In a file: " + name_ofpic)
         image_result.write(previewimage)
 
+        # get all waypoints
         items = mydoc.getElementsByTagName('WayPoint')
-
+        # all all original waypoints
         waypoints = []
-        #maxofwaypoints = 0
+        # maxofwaypoints = 0 not needed, use len(waypoints)
         lastID = 0
 
         # add classes to 'waypoints' array
         for elem in items:
             if elem.getAttribute("spawn") == "Human":
-                #   waypoint[elem] = "Human"
                 job = elem.getAttribute('job')
+                # initialized first to catch errors later
                 x = 0
                 y = 0
                 idcards = "error"
-                found = False
-                # jobs to replace,  assistant split to other classes and awaiting rework
                 if job in vanillaJobs:
                     x = elem.getAttribute('x')
                     y = elem.getAttribute('y')
                     idcardtags = elem.getAttribute('idcardtags')
-                    found = True
                     SpawnPointHuman = {
                         'job': job,
                         'x': x,
@@ -125,7 +123,7 @@ def main():
         # create new waypoints and store them in 'newwaypoints' array
         newwaypoints = []
         for i in range(len(waypoints)):
-            przesuniecie = 40
+            offset = 40
             # capitan -> co + xo + nav
             if waypoints[i]['job'] == 'captain':
                 # add co spawn from variables - same coordinates as capitan
@@ -133,10 +131,10 @@ def main():
                 newwaypoints.append(add_by_id(job, waypoints[i], 0))
                 # add xo spawn from variables - coordinates from co x=x-2
                 job = 'executive_officer'
-                newwaypoints.append(add_by_id(job, waypoints[i], -przesuniecie))
+                newwaypoints.append(add_by_id(job, waypoints[i], -offset))
                 # add nav spawn from variables - coordinates from co x=x+2
                 job = 'navigator'
-                newwaypoints.append(add_by_id(job, waypoints[i], przesuniecie))
+                newwaypoints.append(add_by_id(job, waypoints[i], offset))
             
             # engineer -> engineering + chief
             if waypoints[i]['job'] == 'engineer':
@@ -145,7 +143,7 @@ def main():
                 newwaypoints.append(add_by_id(job, waypoints[i], 0))
                 # add chief spawn from variables - coordinates from engineering x=x-2
                 job = 'chief'
-                newwaypoints.append(add_by_id(job, waypoints[i], -przesuniecie))
+                newwaypoints.append(add_by_id(job, waypoints[i], -offset))
             
             # mechanic -> mechanical + quartermaster
             if waypoints[i]['job'] == 'mechanic':
@@ -154,7 +152,7 @@ def main():
                 newwaypoints.append(add_by_id(job, waypoints[i], 0))
                 # add quartermaster spawn from variables - coordinates from mechanical x=x-2
                 job = 'quartermaster'
-                newwaypoints.append(add_by_id(job, waypoints[i], -przesuniecie))
+                newwaypoints.append(add_by_id(job, waypoints[i], -offset))
             
             # assistant -> passenger + janitor
             if waypoints[i]['job'] == 'assistant':
@@ -163,7 +161,7 @@ def main():
                 newwaypoints.append(add_by_id(job, waypoints[i], 0))
                 # add janitor spawn from variables - coordinates from assistant x=x-2
                 job = 'janitor'
-                newwaypoints.append(add_by_id(job, waypoints[i], -przesuniecie))
+                newwaypoints.append(add_by_id(job, waypoints[i], -offset))
 
             # securityofficer -> securityofficer + head_of_security + diver
             if waypoints[i]['job'] == 'securityofficer':
@@ -172,10 +170,10 @@ def main():
                 newwaypoints.append(add_by_id(job, waypoints[i], 0))
                 # add head_of_security spawn from variables - coordinates from securityofficer x=x-2
                 job = 'head_of_security'
-                newwaypoints.append(add_by_id(job, waypoints[i], -przesuniecie))
+                newwaypoints.append(add_by_id(job, waypoints[i], -offset))
                 # add diver spawn from variables - coordinates from securityofficer x=x+2
                 job = 'diver'
-                newwaypoints.append(add_by_id(job, waypoints[i], przesuniecie))
+                newwaypoints.append(add_by_id(job, waypoints[i], offset))
             
             # medicaldoctor -> medicaldoctor + chiefmedicaldoctor + passenger
             if waypoints[i]['job'] == 'medicaldoctor':
@@ -184,10 +182,10 @@ def main():
                 newwaypoints.append(add_by_id(job, waypoints[i], 0))
                 # add chiefmedicaldoctor spawn from variables - coordinates from medicaldoctor x=x-2
                 job = 'chiefmedicaldoctor'
-                newwaypoints.append(add_by_id(job, waypoints[i], -przesuniecie))
+                newwaypoints.append(add_by_id(job, waypoints[i], -offset))
                 # add passenger spawn from variables - coordinates from medicaldoctor x=x+2
                 job = 'passenger'
-                newwaypoints.append(add_by_id(job, waypoints[i], przesuniecie))
+                newwaypoints.append(add_by_id(job, waypoints[i], offset))
 
 
         # testing 'newwaypoints' array
@@ -208,9 +206,10 @@ def main():
         # change name option
         if changename and name.find(' [JE]') == -1:
             name = name + ' [JE]'
+        mydoc.documentElement.setAttribute('name', name)
         # add JobsExtended to requiredcontentpackages
         if requiredcontentpackages.find('JobsExtended') == -1:
-            requiredcontentpackages = requiredcontentpackages + 'JobsExtended'
+            requiredcontentpackages = requiredcontentpackages + ', JobsExtended'
         mydoc.documentElement.setAttribute('requiredcontentpackages', requiredcontentpackages)
 
         # all items data TESTING
